561978efd857e61743c42eb6162dfa5405587323
Matt Bhagat-Conway <matt@indicatrix.org>
2023-06-30 11:10:09 -0400

    WIP on map-matching

--- cxx/match.cpp
+++ cxx/match.cpp
@@ -16,8 +16,11 @@ using namespace std;
  * Call the OSRM map matcher. "timestamps" should be in seconds since Unix epoch.
  * Returns -1 if OSRM error, 0 otherwise.
  */
-extern "C" int osrm_match (struct osrm::OSRM * osrminst, double * lats, double * lons, unsigned * timestamps, size_t n_points,
-    int (*callback)(osrm::json::Object*, void*), void * result_array) {
+extern "C" int osrm_match (struct osrm::OSRM * osrminst, double * lats, double * lons, size_t n_points,
+    unsigned * timestamps, size_t n_timestamps,
+    double * radii, size_t n_radii,
+    bool tidy, bool annotations, bool steps, bool split_gaps,
+    int (*callback)(osrm::json::Value*, void*), void * result_array) {
     
     using namespace osrm;
 
@@ -25,8 +28,34 @@ extern "C" int osrm_match (struct osrm::OSRM * osrminst, double * lats, double *
 
     for (size_t i = 0; i < n_points; i++) {
         params.coordinates.push_back({util::FloatLongitude{lons[i]}, util::FloatLatitude{lats[i]}});
+    }
+
+    for (size_t i = 0; i < n_timestamps; i++) {
         params.timestamps.push_back(timestamps[i]);
     }
+
+    for (size_t i = 0; i < n_radii; i++) {
+        params.radiuses.push_back(radii[i]);
+    }
+
+    params.tidy = tidy;
+
+    if (annotations) {
+        params.annotations = true;
+        params.annotations_type = RouteParameters::AnnotationsType::All;
+    } else {
+        params.annotations = false;
+    }
+
+
+    params.steps = steps;
+    if (split_gaps) {
+        params.gaps = MatchParameters::GapsType::Split;
+    } else {
+        params.gaps = MatchParameters::GapsType::Ignore;
+    }
+
+    params.overview = RouteParameters::OverviewType::Full;
 
     params.geometries = RouteParameters::GeometriesType::GeoJSON;
 
@@ -34,12 +63,10 @@ extern "C" int osrm_match (struct osrm::OSRM * osrminst, double * lats, double *
 
     Status stat = osrminst->Match(params, result);
 
-    auto &json_result = result.get<json::Object>();
-
     if (stat == Status::Ok) {
-        return callback(&json_result, result_array);
+        return callback(&result, result_array);
     } else {
-        auto code = json_result.values.at("code").get<osrm::json::String>().value.c_str();
+        auto code = result.get<json::Object>().values.at("code").get<osrm::json::String>().value.c_str();
         cout << "OSRM error: " << code << endl;
         return -1;
     }
--- src/match.jl
+++ src/match.jl
@@ -1,36 +1,85 @@
+struct Tracepoint
+    lat::Float64
+    lon::Float64
+    snap_distance_meters::Float64
+    route_index::Int64
+    within_route_index::Int64
+    alternatives_count::Int64
+end
+
 struct MapMatchResult
     routes::Vector{Route}
+    tracepoints::Vector{Union{Nothing, Tracepoint}}
 end
 
 function parse_match(resultptr, res)
-    result::Vector{Route} = unsafe_pointer_to_objref(res)
+    @info "running callback"
 
-    routeptr = json_obj_get_arr(resultptr, "matchings")
-    for i in 0:(json_arr_length(routeptr) - 1)
-        push!(result, parse_route(json_arr_get_obj(routeptr, i)))
-    end
+    resultvec::Vector{MapMatchResult} = unsafe_pointer_to_objref(res)
+    result = resultvec[1]
+
+    routeptr = json_obj_has_key(resultptr, "matchings")
+    # for i in 0:(json_arr_length(routeptr) - 1)
+    #     push!(result.routes, parse_route(json_arr_get_obj(routeptr, i)))
+    # end
     
     return zero(Int32)
 end
 
-function mapmatch(osrm::OSRMInstance, points::Vector{LatLon{Float64}}, timestamps::Vector{DateTime})
-    result = Vector{Route}()
+function parse_tracepoint(tptr)
+    nothing
+end
 
-    length(points) == length(timestamps) || error("Length of points and timestamps must match!")
+function mapmatch(
+        osrm::OSRMInstance,
+        points::AbstractVector{LatLon{Float64}};
+        timestamps::Union{Nothing, <:AbstractVector{DateTime}}=nothing,
+        # should be radii, but we'll match OSRM
+        std_error_meters::Union{Nothing, <:Real, <:AbstractVector{<:Real}}=nothing,
+        tidy=false,
+        annotations=false,
+        steps=false,
+        split_gaps=true
+        )
+    result = MapMatchResult(Route[], Tracepoint[])
 
     parse_match_c = @cfunction(parse_match, Cint, (Ptr{Any}, Ptr{Any}))
+
+    radius_vector = if isnothing(std_error_meters)
+         Float64[]
+    elseif std_error_meters isa Real
+        fill(std_error_meters, length(points))
+    else
+        length(points) == length(std_error_meters) || error("Length of points and timestamps must match!")
+        convert.(Float64, std_error_meters)
+    end
+
+    timestamp_vector = if isnothing(timestamps)
+        DateTime[]
+    else
+        length(points) == length(timestamps) || error("Length of points and timestamps must match!")
+        timestamps
+    end
 
     status = @ccall osrmjl.osrm_match(
         osrm._engine::Ptr{Any},
         [x.lat for x in points]::Ptr{Float64},
         [x.lon for x in points]::Ptr{Float64},
-        round.(UInt32, datetime2unix.(timestamps))::Ptr{UInt32},
         length(points)::Csize_t,
+        round.(UInt32, datetime2unix.(timestamp_vector))::Ptr{UInt32},
+        length(timestamp_vector)::Csize_t,
+        radius_vector::Ptr{Float64},
+        length(radius_vector)::Csize_t,
+        tidy::Bool,
+        annotations::Bool,
+        steps::Bool,
+        split_gaps::Bool,
         parse_match_c::Ptr{Any},
-        pointer_from_objref(result)::Ptr{Any}
+        # enclosing vector becasue we can only pass mutable objs through ccall
+        pointer_from_objref([result])::Ptr{Any}
         )::Cint
 
     status == 0 || error("OSRM error")
 
-    return MapMatchResult(result)
+    return result
 end
